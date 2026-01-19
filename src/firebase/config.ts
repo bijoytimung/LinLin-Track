9926ab5ad22a2875d8386e4cfaf49657ac2afb50
Bijoy Timung <tnaharsing@gmail.com>
2026-01-19 18:27:12 +0000

    Update firebase config with storage

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
-export const firebaseConfig = {
-  "projectId": "studio-9792727875-b4105",
-  "appId": "1:228202722004:web:fafd717f6ff8b769ec0141",
-  "apiKey": "AIzaSyCo-ToELvqtd-bw8rHBD4qNKzwMdaC72xA",
-  "authDomain": "studio-9792727875-b4105.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "228202722004"
+export const config = {
+  apiKey: "AIzaSyCo-ToELvqtd-bw8rHBD4qNKzwMdaC72xA",
+  authDomain: "studio-9792727875-b4105.firebaseapp.com",
+  projectId: "studio-9792727875-b4105",
+  storageBucket: "studio-9792727875-b4105.firebasestorage.app",
+  messagingSenderId: "228202722004",
+  appId: "1:228202722004:web:fafd717f6ff8b769ec0141",
+  measurementId: ""
 };
